29363d77d2c63895a03742bf992686d5775ff6d1
AUREL JIHAN FAHRISTY_2301020013 <aureljihan05@gmail.com>
2026-06-02 08:55:44 +0700

    Update predict_species.py

--- utils/predict_species.py
+++ utils/predict_species.py
@@ -1,30 +1,21 @@
-from tensorflow.keras.models import load_model
 import numpy as np
+import tensorflow as tf
 
 from utils.preprocess import preprocess_image
 from utils.labels import SPECIES_LABELS
 from config import Config
 
+# Load model sekali saat startup
 model = tf.keras.models.load_model(
     Config.SPECIES_MODEL_PATH,
     compile=False,
-    options=tf.saved_model.LoadOptions(
-        experimental_io_device='/job:localhost'
-    )
+    custom_objects={'InputLayer': tf.keras.layers.InputLayer}
 )
 
 def predict_species(img_path):
-    """
-    Menjalankan Model Tahap 1: Klasifikasi Spesies.
-    Menggunakan softmax — nilai tiap kelas mewakili probabilitas.
-    Mengembalikan: (nama_spesies, confidence_score)
-    """
     processed_image = preprocess_image(img_path)
-
-    prediction = model.predict(processed_image)  # shape: (1, 3)
-
+    prediction = model.predict(processed_image)
     predicted_index = int(np.argmax(prediction))
     confidence = float(np.max(prediction))
     species = SPECIES_LABELS[predicted_index]
-
     return species, confidence
